c6428833e6f7296c5d48a364cffe1c35474928d2
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-01-20 17:16:26 +0200

    lesson2-by-pavel

--- src/lesson-2/calc/math-operators/mathOperators.test.ts
+++ src/lesson-2/calc/math-operators/mathOperators.test.ts
@@ -1,31 +1,32 @@
 import { mul, div, add, minus, pow } from "./mathOperators";
 
-describe("mathOperators test cases", () => {
-  let number = Math.floor(Math.random() * 20) + 1;
-  let number1 = Math.floor(Math.random() * 6) + 1;
+describe("mathOperators it cases", () => {
+  const number = Math.floor(Math.random() * 20) + 1;
+  const number1 = Math.floor(Math.random() * 6) + 1;
   const multiplyResult = number * number1;
   const divResult = number / number1;
   const addResult = number + number1;
-  const minusResult = number - number1; 
-  const resultOfPow = Math.pow(number1, number1);
+  const minusResult = number - number1;
+  const resultOfPow = Math.pow(number, number1);
 
-  test(`mul ${number} * ${number1} to equal ${multiplyResult}`, () => {
+  it(`mul ${number} * ${number1} to equal ${multiplyResult}`, () => {
     expect(mul(number, number1)).toBe(multiplyResult);
   });
 
-  test(`div ${number} / ${number1} to equal ${divResult}`, () => {
+  it(`div ${number} / ${number1} to equal ${divResult}`, () => {
     expect(div(number, number1)).toBe(divResult);
   });
 
-  test(`add ${number} / ${number1} to equal ${addResult}`, () => {
+  it(`add ${number} / ${number1} to equal ${addResult}`, () => {
     expect(add(number, number1)).toBe(addResult);
   });
 
-  test(`min ${number} - ${number1} to equal ${minusResult}`, () => {
+  it(`min ${number} - ${number1} to equal ${minusResult}`, () => {
     expect(minus(number, number1)).toBe(minusResult);
   });
 
-  test(`pow ${number} ** ${number1} to equal ${resultOfPow}`, () => {
+  it(`pow ${number} ** ${number1} to equal ${resultOfPow}`, () => {
     expect(pow(number, number1)).toBe(resultOfPow);
   });
 });
+it;
--- src/lesson-2/calc/math-operators/mathOperators.ts
+++ src/lesson-2/calc/math-operators/mathOperators.ts
@@ -1,21 +1,26 @@
 export type ScalarOperationType = (first: number, second: number) => number;
 
-export const mul: ScalarOperationType = (first, second): number => first * second;
+export const mul: ScalarOperationType = (first, second): number =>
+  first * second;
 
-export const div: ScalarOperationType = (first, second): number => first / second;
+export const div: ScalarOperationType = (first, second): number =>
+  first / second;
 
-export const add: ScalarOperationType = (first, second): number => first + second;
+export const add: ScalarOperationType = (first, second): number =>
+  first + second;
 
-export const minus: ScalarOperationType = (first, second): number => first - second;
+export const minus: ScalarOperationType = (first, second): number =>
+  first - second;
 
-export const pow: ScalarOperationType = (first, second) => Math.pow(first, second);
+export const pow: ScalarOperationType = (first, second) =>
+  Math.pow(first, second);
 
 export const mathOperators: { [key: string]: ScalarOperationType } = {
   "*": mul,
   "/": div,
   "+": add,
   "-": minus,
-  "**": pow
+  "**": pow,
 };
 
 export const mathPriorities: Array<number> = [1, 2];
